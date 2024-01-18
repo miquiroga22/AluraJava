3ba9d6428ddb46f148d6b69c9a8a38ecf212a292
Alex Quiroga <miquiroga22@gmail.com>
2024-01-17 19:02:57 -0500

    commit primer proyecto alura java

---
+++ app.js
@@ -0,0 +1,37 @@
+let numeroSecreto = 6; 
+
+let numeroUsuario = prompt('Me indicas un número por favor');
+
+ console.log(numeroUsuario);
+
+ if (numeroUsuario == numeroSecreto){
+  alert('Acertaste el numero');
+ }
+
+//Desafio: hora de practicar
+// Muestra una alerta de bienvenida
+alert("¡Bienvenida y bienvenido a nuestro sitio web!");
+// Declara la variable nombre y asigna el valor "Luna"
+var nombre = "Luna";
+// Crea la variable edad y asigna el valor 25
+var edad = 25;
+// Establece la variable numeroDeVentas y asigna el valor 50
+var numeroDeVentas = 50;
+// Establece la variable saldoDisponible y asigna el valor 1000
+var saldoDisponible = 1000;
+// Muestra una alerta con el texto "¡Error! Completa todos los campos"
+alert("¡Error! Completa todos los campos");
+// Declara la variable mensajeDeError y asigna el valor "¡Error! Completa todos los campos"
+var mensajeDeError = "¡Error! Completa todos los campos";
+// Muestra una alerta con el valor de la variable mensajeDeError
+alert(mensajeDeError);
+// Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre
+nombre = prompt("Por favor, ingresa tu nombre:");
+// Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad
+edad = prompt("Por favor, ingresa tu edad:");
+// Convierte la entrada de edad a un número
+edad = parseInt(edad);
+// Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!"
+if (edad >= 18) {
+  alert("¡Puedes obtener tu licencia de conducir!");
+}
